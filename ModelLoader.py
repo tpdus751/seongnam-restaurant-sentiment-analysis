94c174e2163caf5269696773cc6fcd72dcba35a7
tpdus751 <751psy@naver.com>
2025-06-25 06:07:42 +0900

    1235666

--- ModelLoader.py
+++ ModelLoader.py
@@ -1,9 +1,9 @@
+# ModelLoader.py
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
 import streamlit as st
 
 @st.cache_resource
-def load_model(model_path="./kc_electra_sentiment_model_0624_3"):
+def load_model(model_path):
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     model = AutoModelForSequenceClassification.from_pretrained(model_path)
     return tokenizer, model
